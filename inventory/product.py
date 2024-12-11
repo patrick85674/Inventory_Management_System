1d4863f7ca452a697bc696d43a52366ea2fca8d7
Fikret <fikret.veseli@dci-student.org>
2024-12-11 14:27:21 +0100

    Modified Product class

--- inventory/product.py
+++ inventory/product.py
@@ -21,12 +21,28 @@ class Product:
     def name(self, name: str):
         self.__name = name
 
+    @property
+    def price(self) -> float:
+        return self.__price
+
+    @property
+    def quantity(self) -> int:
+        return self.__quantity
+
+    @property
+    def category(self) -> str:
+        return self.__category
+    
     def update_quantity(self, quantity: int):
         """Update the quantity of the product."""
+        if quantity < 0:
+            raise ValueError("Quantity cannot be negative.")
         self.__quantity = quantity
-
+        
     def update_price(self, price: float):
         """Update the price of the product."""
+        if price < 0:
+            raise ValueError("Price cannot be negative.")
         self.__price = price
 
     def update_category(self, category: str):
@@ -35,5 +51,5 @@ class Product:
 
     def get_info(self) -> str:
         """Return a string representation of the product's information."""
-        return (f"Product: {self.__name}, price: {self.__price},"
+        return (f"ProductID: {self.__id}, Product name: {self.__name}, price: {self.__price},"
                 f" quantity: {self.__quantity}, category: {self.__category}")
